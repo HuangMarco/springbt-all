75d65558e66acbb6e572332c43182a77f5269d66
HuangMarco <ht576475012@163.com>
2020-06-27 23:42:28 +0800

    [init] init using the sacffold of the sb:wuji-system/spring-knowledge/springboot/springboot-app-scaffold

---
+++ src/main/java/com/huangshi/wuji/spring/scaffold/SpringScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.huangshi.wuji.spring.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huangshi/wuji/spring/scaffold/SpringScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangshi.wuji.spring.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
