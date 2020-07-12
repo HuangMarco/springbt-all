912eef471544b95932d05e8a41e2f05de0bd7602
HuangMarco <ht576475012@163.com>
2020-07-12 23:45:04 +0800

    [update] updated client of dynamicproxy

--- src/main/java/com/huangshi/wuji/spring/scaffold/designpattern/dynamicproxy/Client.java
+++ src/main/java/com/huangshi/wuji/spring/scaffold/designpattern/dynamicproxy/Client.java
@@ -13,6 +13,7 @@ public class Client {
         JDKDynamicProxy proxy = new JDKDynamicProxy(carTarget);
 
         // JDK 动态代理是基于接口的，根据代理进一步生成基于接口的代理对象
+        //相当于直接调用Proxy.newProxyInstance方法，只不过此时JDKDynamicProxy里面已经有所有需要的东西了,因为你是通过getProxy()方法调用的
         Vehicle carProxy = proxy.getProxy();
 
         //调用代理对象的方法
