e19475c2f0547e390d3c988831054fb32ca42906
HuangMarco <ht576475012@163.com>
2020-07-15 01:49:02 +0800

    [update] updated banner

--- src/main/java/com/huangshi/wuji/spring/scaffold/banner/ASCIIArtService.java
+++ src/main/java/com/huangshi/wuji/spring/scaffold/banner/ASCIIArtService.java
@@ -8,18 +8,18 @@ public class ASCIIArtService {
 
     public static void main(String[] args) throws IOException {
 
-        int width = 100;
-        int height = 30;
+        int width = 150;
+        int height = 20;
 
         //BufferedImage image = ImageIO.read(new File("/Users/mkyong/Desktop/logo.jpg"));
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 24));
+        g.setFont(new Font("SansSerif", Font.BOLD, 25));
 
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("Huang", 10, 20);
+        graphics.drawString("HUANG ZU", 10, 20);
 
         //save this image
         //ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
